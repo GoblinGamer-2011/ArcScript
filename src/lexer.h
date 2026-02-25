92a2c3139d6c5ea07f25725b6df8524406681bb1
GoblinGamer-2011 <alexfellows964@gmail.com>
2026-02-25 19:20:03 +0000

    Update lexer.h

--- src/lexer.h
+++ src/lexer.h
@@ -1,1 +1,17 @@
+#ifndef LEXER_H
+#define LEXER_H
 
+#include "token.h"
+
+typedef struct {
+    const char *src;
+    int pos;
+    int line;
+    int column;
+    char current;
+} Lexer;
+
+void lexer_init(Lexer *lexer, const char *source);
+Token lexer_next_token(Lexer *lexer);
+
+#endif
