6a37e6347ad083f32bf816c209d306440c505116
GoblinGamer-2011 <alexfellows964@gmail.com>
2026-02-25 19:32:28 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lexer.h"
 
 int main() {
 
-    const char *src =
+    const char *source =
         "var x = 4;\n"
-        "x += 1;\n";
+        "x += 1;\n"
+        "// comment\n"
+        "const y = 10;\n";
 
     Lexer lexer;
-    lexer_init(&lexer, src);
+    lexer_init(&lexer, source);
 
     Token tok;
 
     do {
         tok = lexer_next_token(&lexer);
-        printf("Token: %d (%s)\n", tok.type, tok.lexeme);
+
+        printf(
+            "Token %-3d | %-10s | line %d col %d\n",
+            tok.type,
+            tok.lexeme,
+            tok.line,
+            tok.column
+        );
+
     } while (tok.type != TOKEN_EOF);
 
     return 0;
